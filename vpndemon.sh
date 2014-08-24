cc091a74ac6e4ddb8ffec77e6b5c6cd6aa68e502
K <kbecker@primaryobjects.com>
2014-08-24 11:31:40 -0500

    Removed unused code.

--- vpndemon.sh
+++ vpndemon.sh
@@ -3,13 +3,12 @@
 killProgramName="calc"
 interface="org.freedesktop.NetworkManager.VPN.Connection"
 member="VpnStateChanged"
-#isDone=0
 
 # Monitor for VPNStateChanged event.
 dbus-monitor --system "type='signal',interface='$interface',member='$member'" |
 {
     # Read output from dbus.
-    while read -r line #&& [ $isDone = 0 ]
+    while read -r line
     do
         # Check if this a VPN disconnected (uint32 7) event.
         if [ x"$(echo "$line" | grep 'uint32 7')" != x ]
@@ -19,20 +18,12 @@ dbus-monitor --system "type='signal',interface='$interface',member='$member'" |
 
             echo "VPN Disconnected!"
 
-            # Disconnect network.
-            # nmcli -p nm enable false
-
             # Kill target process.
             for i in `pgrep $killProgramName`
             do
                 echo "Terminated $i."
                 kill $i
             done
-
-            #isDone=1
         fi
     done
-
-    # Kill all threads and exit script.
-    #kill $$
 }
