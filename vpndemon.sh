f0b5613afb8712dec45b4ab03aaf8246bb9877b9
K <kbecker@primaryobjects.com>
2014-08-24 21:08:50 -0500

    Added UI to enter process name to kill.

--- vpndemon.sh
+++ vpndemon.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-killProgramName="calc"
 interface="org.freedesktop.NetworkManager.VPN.Connection"
 member="VpnStateChanged"
 logPath="/tmp/vpndemon"
@@ -9,52 +8,58 @@ header="VPNDemon\nhttps://github.com/primaryobjects/vpndemon\n\n"
 # Clear log file.
 > "$logPath"
 
-(tail -f "$logPath") |
-{
-    zenity --progress --title="VPNDemon" --text="$header Monitoring VPN" --pulsate
-
-    # Kill all child processes upon exit. kill 0 sends a SIGTERM to the whole process group, thus killing also descendants.
-    trap "kill 0" SIGINT SIGTERM EXIT
-
-    # Terminate app.
-    kill $$
-} |
-{
-    # Monitor for VPNStateChanged event.
-    dbus-monitor --system "type='signal',interface='$interface',member='$member'" |
+killProgramName=$(zenity --entry --title="VPNDemon" --text="$header Enter name of process to kill when VPN disconnects:")
+if [ $killProgramName ]
+then
+    (tail -f "$logPath") |
     {
-        # Read output from dbus.
-        (while read -r line
-        do
-            currentDate=`date +'%m-%d-%Y %r'`
+        zenity --progress --title="VPNDemon" --text="$header Monitoring VPN" --pulsate
 
-            # Check if this a VPN connection (uint32 2) event.
-            if [ x"$(echo "$line" | grep 'uint32 3')" != x ]
-            then
-                echo "VPN Connected $currentDate"
-                echo "# $header VPN Connected $currentDate" >> "$logPath"
-            fi
+        # Kill all child processes upon exit. kill 0 sends a SIGTERM to the whole process group, thus killing also descendants.
+        trap "kill 0" SIGINT SIGTERM EXIT
 
-            # Check if this a VPN disconnected (uint32 7) event.
-            if [ x"$(echo "$line" | grep 'uint32 7')" != x ]
-            then
-                echo "VPN Disconnected $currentDate"
-                echo "# $header VPN Disconnected $currentDate" >> "$logPath"
+        # Terminate app.
+        kill $$
+    } |
+    {
+        # Monitor for VPNStateChanged event.
+        dbus-monitor --system "type='signal',interface='$interface',member='$member'" |
+        {
+            # Read output from dbus.
+            (while read -r line
+            do
+                currentDate=`date +'%m-%d-%Y %r'`
 
-                # Kill target process.
-                for i in `pgrep $killProgramName`
-                do
-                    # Get process name.
-                    name=$(ps -ocommand= -p $i | awk -F/ '{print $NF}' | awk '{print $1}')
+                # Check if this a VPN connection (uint32 2) event.
+                if [ x"$(echo "$line" | grep 'uint32 3')" != x ]
+                then
+                    echo "VPN Connected $currentDate"
+                    echo "# $header VPN Connected $currentDate" >> "$logPath"
+                fi
 
-                    # Kill process.
-                    kill $i
+                # Check if this a VPN disconnected (uint32 7) event.
+                if [ x"$(echo "$line" | grep 'uint32 7')" != x ]
+                then
+                    echo "VPN Disconnected $currentDate"
+                    echo "# $header VPN Disconnected $currentDate" >> "$logPath"
 
-                    # Log result.
-                    echo "Terminated $i ($name)"
-                    echo "Terminated $i ($name)" >> "$logPath"
-                done
-            fi
-        done)
+                    # Kill target process.
+                    for i in `pgrep $killProgramName`
+                    do
+                        # Get process name.
+                        name=$(ps -ocommand= -p $i | awk -F/ '{print $NF}' | awk '{print $1}')
+
+                        # Kill process.
+                        kill $i
+
+                        # Log result.
+                        echo "Terminated $i ($name)"
+                        echo "Terminated $i ($name)" >> "$logPath"
+                    done
+                fi
+            done)
+        }
     }
-}
+else
+    zenity --error --text="No process name entered."
+fi
